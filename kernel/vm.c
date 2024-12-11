ab2845535fff0e83a43d7e6b2c03b181ac21befb
Maxmilite <yufe008@live.com>
2024-12-12 02:11:45 +0800

    printout

--- kernel/vm.c
+++ kernel/vm.c
@@ -449,3 +449,27 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+void vmprint(pagetable_t pgtbl) {
+  printf("page table %p\n", pgtbl);
+  for (int i = 0; i < 512; i++) {
+    pte_t pte = pgtbl[i];
+    if (pte & PTE_V) {
+      printf("..%d: pte %p pa %p\n", i, pte, PTE2PA(pte));
+      pagetable_t pgtbl2 = (pagetable_t)PTE2PA(pte);
+      for (int j = 0; j < 512; j++) {
+        pte_t pte2 = pgtbl2[j];
+        if (pte2 & PTE_V) {
+          printf(".. ..%d: pte %p pa %p\n", j, pte2, PTE2PA(pte2));
+          pagetable_t pgtbl3 = (pagetable_t)PTE2PA(pte2);
+          for (int k = 0; k < 512; k++) {
+            pte_t pte3 = pgtbl3[k];
+            if (pte3 & PTE_V) {
+              printf(".. .. ..%d: pte %p pa %p\n", k, pte3, PTE2PA(pte3));
+            }
+          }
+        }
+      }
+    }
+  }
+}
