f5966ef8d0bd896c8200d641d928bb08270daa48
Maxmilite <yufe008@live.com>
2024-12-12 20:39:08 +0800

    stash

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -9,6 +9,9 @@
 #include "riscv.h"
 #include "defs.h"
 
+int refcount[PHYSTOP / PGSIZE];
+struct spinlock refcount_lock;
+
 void freerange(void *pa_start, void *pa_end);
 
 extern char end[]; // first address after kernel.
@@ -51,6 +54,13 @@ kfree(void *pa)
   if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
     panic("kfree");
 
+  acquire(&refcount_lock);
+  if (--refcount[(uint64)pa / PGSIZE] > 0) {
+    release(&refcount_lock);
+    return;
+  }
+  release(&refcount_lock);
+
   // Fill with junk to catch dangling refs.
   memset(pa, 1, PGSIZE);
 
@@ -72,8 +82,12 @@ kalloc(void)
 
   acquire(&kmem.lock);
   r = kmem.freelist;
-  if(r)
+  if(r) {
     kmem.freelist = r->next;
+    acquire(&refcount_lock);
+    refcount[(uint64)r / PGSIZE] = 1;
+    release(&refcount_lock);
+  }
   release(&kmem.lock);
 
   if(r)
